5f2bed58c6f42e9b7bb3234cb0ca1f158c4672ee
shiwook_github_01 <shiwook_github_01@hotmail.com>
2022-03-26 19:06:36 +0900

    first commit

---
+++ scoop-windows-terminal/windows-terminal-installer.ps1
@@ -0,0 +1,6 @@
+# windows-terminal 설치 실행 가이드.
+# https://github.com/microsoft/terminal
+
+# 1) scoop installation.
+scoop bucket add extras
+scoop install windows-terminal
---
+++ scoop-windows-terminal/windows-terminal-update-checker.ps1
@@ -0,0 +1,2 @@
+# 1) scoop windows-terminal check lastest update.
+scoop update windows-terminal
